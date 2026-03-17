5baa05d1f3b0075eab1fe64849e7369bc135cdec
Igor Gonçalves <igorjulio@gmail.com>
2026-03-17 00:30:19 -0300

    chore(deploy): configurar conexão do banco de dados de produção

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=controle_estoque',
     'username' => 'estoque_user',
-    'password' => 'SUA_SENHA',
+    'password' => 'REMOVIDO',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
