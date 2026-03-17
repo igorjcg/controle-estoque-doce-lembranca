c67237a3cb7a90d7ec2dffbc396300af202f8901
Igor Gonçalves <igorjulio@gmail.com>
2026-03-17 00:53:56 -0300

    feat: adicionar configuração de banco local (db-local.php) e ignorar no git

--- config/db.php
+++ config/db.php
@@ -1,5 +1,9 @@
 <?php
 
+if (file_exists(__DIR__ . '/db-local.php')) {
+    return require __DIR__ . '/db-local.php';
+}
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=controle_estoque',
