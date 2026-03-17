68e42ec8249ebced44630e14469b2083a93faee2
Igor Gonçalves <igorjulio@gmail.com>
2026-03-17 10:07:56 -0300

    ajusta db para uso com dotenv

--- config/db.php
+++ config/db.php
@@ -7,8 +7,8 @@ if (file_exists(__DIR__ . '/db-local.php')) {
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=controle_estoque',
-    'username' => 'estoque_user',
-    'password' => 'REMOVIDO',
+	'username' => getenv('DB_USER'),
+	'password' => getenv('DB_PASS'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
