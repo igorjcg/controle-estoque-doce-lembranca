f81b969d21f96a370c4b519689145ab20d6da3cc
Igor Gonçalves <igorjulio@gmail.com>
2026-03-17 11:46:07 -0300

    add dotenv configuration

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
 use Dotenv\Dotenv;
-
-
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 $dotenv->required(['DB_USER', 'DB_PASS'])->notEmpty();
