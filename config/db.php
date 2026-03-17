935a7d6e378fcf4bf6f3d966a423c95e6027a96b
Igor Gonçalves <igorjulio@gmail.com>
2026-03-17 10:23:05 -0300

    config: integração segura com dotenv (sem credenciais no código)

--- config/db.php
+++ config/db.php
@@ -7,8 +7,8 @@ if (file_exists(__DIR__ . '/db-local.php')) {
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=controle_estoque',
-	'username' => getenv('DB_USER'),
-	'password' => getenv('DB_PASS'),
+	'username' => $_ENV['DB_USER'] ?? null,
+	'password' => $_ENV['DB_PASS'] ?? null,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- web/index.php
+++ web/index.php
@@ -5,6 +5,12 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
+
+$dotenv->required(['DB_USER', 'DB_PASS'])->notEmpty();
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
